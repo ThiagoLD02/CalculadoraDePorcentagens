43f0a7aa7951c2d793134336939e0c92a8497c0e
ThiagoLD02 <thiagoladainha@gmail.com>
2021-01-05 16:08:18 -0300

    Trocar states para arrays

---
+++ src/history.tsx
@@ -0,0 +1,20 @@
+import styles from './App.module.css';
+interface type{
+    percentage : number,
+    money: number,
+    value:number
+}
+
+function History(params:type){
+    var boxes = () => { 
+      <div className={styles.content}>
+        {params.value}+{params.percentage}% = {params.money}
+      </div>
+    }
+    return(
+      <div className={styles.historyBox}>
+        {boxes};
+      </div>
+    );
+  }
+export default History;
